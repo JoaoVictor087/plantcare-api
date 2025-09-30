58fb17a53c5bb3cd093e12dedf1da62255271f29
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-30 10:31:10 -0300

    Primeiro Commit

---
+++ src/main/java/com/plantcare/plantcare_api/PlantcareApiApplication.java
@@ -0,0 +1,13 @@
+package com.plantcare.plantcare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantcareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantcareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantcare/plantcare_api/PlantcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantcare.plantcare_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
